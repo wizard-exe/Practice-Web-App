ed3dbd1ed82943e680a46574ef3db33c7b3d9e6e
Maximilian Krug <58868250+wizard-exe@users.noreply.github.com>
2025-03-24 20:06:27 +0100

    uibuild

--- backend/models/note.js
+++ backend/models/note.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 
-const url = process.env.MONGODB_URI
+const url = String(process.env.MONGODB_URI)
 
 console.log('connecting to', url)
 mongoose.connect(url)
