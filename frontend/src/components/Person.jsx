0c66fcb1283776769458999dfcc2256192f98089
Maximilian Krug <58868250+wizard-exe@users.noreply.github.com>
2025-03-24 17:55:41 +0100

    new

--- frontend/src/components/Person.jsx
+++
@@ -1,9 +0,0 @@
-const Person = ({ person, deleteP }) => {
-    return (
-        <div>
-            {person.name} {person.number} <button onClick={() => deleteP(person.id)}>delete</button>
-        </div>
-    )
-}
-  
-export default Person
